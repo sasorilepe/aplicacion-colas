b9c5b2c71a53de6634f5d7cbda01e40822117199
Alfredo Alvarado Lepe <alfredo.alvarado@upaep.edu.mx>
2018-11-28 20:35:38 -0600

    Primer commit

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,22 @@
+var socket = io();
+
+var label = $('#lblNuevoTicket');
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Perdimos conexión con el servidor');
+});
+
+socket.on('estadoActual', function(estado) {
+    label.text(estado.actual);
+});
+
+$('button').on('click', function() {
+
+    socket.emit('siguienteTicket', null, function(siguienteTicket) {
+        label.text(siguienteTicket);
+    });
+});
